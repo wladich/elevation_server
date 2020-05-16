5e78e29a293a0b8b625916c9b197c1f18e2caae8
Sergej Orlov <wladimirych@gmail.com>
2020-05-16 18:59:20 +0200

    restructure project for multiple executables

--- cmd/elevation_server/dem_tile.go
+++ cmd/elevation_server/dem_tile.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"github.com/wladich/elevation_server/pkg/constants"
 	"math"
 	"reflect"
 	"unsafe"
 )
 
 const NoValue = -32768
-const HgtSize = 1201
-const HgtSplitParts = 4
-const TileSize = (HgtSize-1)/HgtSplitParts + 1
 
 type TileIndex struct {
 	x, y int
@@ -28,18 +26,18 @@ func bytesToInt16(b []byte) []int16 {
 }
 
 func (tile *Tile) getInterpolated(latlon LatLon) float64 {
-	x := (latlon.lon*HgtSplitParts - float64(tile.index.x)) * (TileSize - 1)
-	y := (latlon.lat*HgtSplitParts - float64(tile.index.y)) * (TileSize - 1)
+	x := (latlon.lon*constants.HgtSplitParts - float64(tile.index.x)) * (constants.TileSize - 1)
+	y := (latlon.lat*constants.HgtSplitParts - float64(tile.index.y)) * (constants.TileSize - 1)
 	indX1 := int(math.Floor(x))
 	indY1 := int(math.Floor(y))
 	indX2 := indX1 + 1
 	indY2 := indY1 + 1
 	dx := x - float64(indX1)
 	dy := y - float64(indY1)
-	v1 := tile.data[indX1+indY1*TileSize]
-	v2 := tile.data[indX2+indY1*TileSize]
-	v3 := tile.data[indX1+indY2*TileSize]
-	v4 := tile.data[indX2+indY2*TileSize]
+	v1 := tile.data[indX1+indY1*constants.TileSize]
+	v2 := tile.data[indX2+indY1*constants.TileSize]
+	v3 := tile.data[indX1+indY2*constants.TileSize]
+	v4 := tile.data[indX2+indY2*constants.TileSize]
 	if v1 == NoValue || v2 == NoValue || v3 == NoValue || v4 == NoValue {
 		return NoValue
 	}
@@ -50,7 +48,7 @@ func (tile *Tile) getInterpolated(latlon LatLon) float64 {
 }
 
 func tileIndexFromLatLon(latlon LatLon) TileIndex {
-	x := int(math.Floor(latlon.lon * HgtSplitParts))
-	y := int(math.Floor(latlon.lat * HgtSplitParts))
+	x := int(math.Floor(latlon.lon * constants.HgtSplitParts))
+	y := int(math.Floor(latlon.lat * constants.HgtSplitParts))
 	return TileIndex{x, y}
 }
---
+++ pkg/constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+const HgtSize = 1201
+const HgtSplitParts = 4
+const TileSize = (HgtSize-1)/HgtSplitParts + 1
